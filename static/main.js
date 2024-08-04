50634e90a3da61f317f08ac037438b3f7bc59a62
Gavin Brown <iamslowtrain@gmail.com>
2024-08-04 19:50:53 +0100

    Begin to replace placeholder text with non-finalised text. Change navbar brand font to Caveat

--- static/main.js
+++ static/main.js
@@ -2,20 +2,20 @@
 
 const alertPlaceholder = document.getElementById('cookieStatement')
 const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
 
-  alertPlaceholder.append(wrapper)
+    alertPlaceholder.append(wrapper)
 }
 
 const alertTrigger = document.getElementById('cookieButton')
 if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    appendAlert('Only essential browser cookies are used ie. for Google Maps. This site does not track you. By using this site you are agreeing to the use of these cookies.', 'warning')
-  })
+    alertTrigger.addEventListener('click', () => {
+        appendAlert('Only essential browser cookies are used ie. for Google Maps. This site does not track you. By using this site you are agreeing to the use of these cookies.', 'warning')
+    })
 }
