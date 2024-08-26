de94b856be500847a6e1fc9c5bb0d8875247549e
Gavin Brown <iamslowtrain@gmail.com>
2024-08-26 15:06:46 +0100

    style embedded Google Map

--- static/main.js
+++ static/main.js
@@ -16,7 +16,7 @@ const appendAlert = (message, type) => {
 const alertTrigger = document.getElementById('cookieButton')
 if (alertTrigger) {
     alertTrigger.addEventListener('click', () => {
-        appendAlert("This site does not use cookies. The embedded Google Map uses the latest Javascript API which doesn't uses cookies", 'warning')
+        appendAlert("The embedded Google Map uses the latest Javascript API which doesn't uses cookies. This site does not use tracking cookies of any kind", 'warning')
     })
 }
 
