07691e6876013212e16d7138dfe00d93b1b89c34
Gavin Brown <iamslowtrain@gmail.com>
2024-09-07 09:41:26 +0100

    adjust cookie statement Javascript

--- static/main.js
+++ static/main.js
@@ -1,27 +1,25 @@
 // Modified Bootstrap Javascript for cookie alert //
-
-const alertPlaceholder = document.getElementById('cookieStatement')
-const appendAlert = (message, type) => {
-    const wrapper = document.createElement('div')
-    wrapper.innerHTML = [
-        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+    const appendAlert = (message, type) => {
+      const wrapper = document.createElement('div')
+      wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible text-center" role="alert">`,
         `   <div>${message}</div>`,
         '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
         '</div>'
-    ].join('')
-
-    alertPlaceholder.append(wrapper)
-}
-
-const alertTrigger = document.getElementById('cookieButton')
-if (alertTrigger) {
-    alertTrigger.addEventListener('click', () => {
+      ].join('')
+    
+      alertPlaceholder.append(wrapper)
+    }
+    
+    const alertTrigger = document.getElementById('liveAlertBtn')
+    if (alertTrigger) {
+      alertTrigger.addEventListener('click', () => {
         appendAlert("This site is designed to be cookie free. This site does not use any analytics and does not track you. Linked third party sites will have their own cookie policies.", 'warning')
-    })
-}
+      })
+    }
 
 /*  ** NEEDS TO BE ADJUSTED ** function to send an email via emailjs */
-
 function sendMail(contactForm) {
     emailjs.send("gmail","4uxdesign", {
         "from_name": contactForm.name.value,
