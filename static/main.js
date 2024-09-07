3e59d1c547465a115005e154a1dcf46b2bc8d882
Gavin Brown <iamslowtrain@gmail.com>
2024-09-07 19:08:27 +0100

    format Javascript

--- static/main.js
+++ static/main.js
@@ -1,20 +1,20 @@
 // Modified Bootstrap Javascript for cookie alert //
     const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
     const appendAlert = (message, type) => {
-      const wrapper = document.createElement('div')
-      wrapper.innerHTML = [
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
         `<div class="alert alert-${type} alert-dismissible text-center" role="alert">`,
         `   <div>${message}</div>`,
         '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
         '</div>'
-      ].join('')
+        ].join('')
     
-      alertPlaceholder.append(wrapper)
+        alertPlaceholder.append(wrapper)
     }
     
     const alertTrigger = document.getElementById('liveAlertBtn')
     if (alertTrigger) {
-      alertTrigger.addEventListener('click', () => {
+        alertTrigger.addEventListener('click', () => {
         appendAlert("This site is designed to be cookie free. This site does not use any analytics and does not track you. Linked third party sites will have their own cookie policies.", 'warning')
-      })
+        })
     }
